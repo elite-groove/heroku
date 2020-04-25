f40b247e9279b07b44a3c558d3a544c89eaca599
Luis Constante <luisconstante@LConstanteOSX.local>
2020-04-24 22:24:05 -0700

    BECAUSE, FUCK HEROKU.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var http = require('http'),
+    fs = require('fs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(process.env.PORT);
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function (request, response) {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+    }).listen(process.env.PORT);
+});
