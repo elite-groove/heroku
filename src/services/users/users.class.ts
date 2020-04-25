5d27f12e57a6c62fe01c04521b8cca4ed6df7315
Luis Constante <luisconstante@LConstanteOSX.local>
2020-04-24 22:27:40 -0700

    FUCK HEROKU. MY FINAL MESSAGE. GOODBYE.

--- src/services/users/users.class.ts
+++
@@ -1,8 +0,0 @@
-import { Service, NedbServiceOptions } from 'feathers-nedb';
-import { Application } from '../../declarations';
-
-export class Users extends Service {
-  constructor(options: Partial<NedbServiceOptions>, app: Application) {
-    super(options);
-  }
-};
